99bd409b23cb3e9654f7c8223df4107e6e3cdb71
Mohamed Kamal <mohamedjop0100@gmail.com>
2025-09-15 06:55:19 -0700

    add project files and updates

--- app/componads/detailsCompnads/collection.jsx
+++ app/componads/detailsCompnads/collection.jsx
@@ -6,6 +6,7 @@ import 'swiper/css/pagination';
 import { Parallax, Pagination  } from 'swiper/modules';
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Collection({collection}){
     const [screenWidth, setScreenWidth] = useState(0);
@@ -21,7 +22,7 @@ export default function Collection({collection}){
             {collection && Object.keys(collection).length > 0 ? (
                     <section>
                         <div className="mt-30" >
-                            <div className='container h-[600px] m-auto'>
+                            <div className='container h-[1000px] m-auto'>
                                 <div 
                                     className="flex size-full flex-col p-5" 
                                 >
@@ -58,22 +59,24 @@ export default function Collection({collection}){
                                             {collection.parts.map((el) => {
                                                 return (
                                                     <SwiperSlide key={el.id} className="flex justify-between flex-row" >
-                                                        <div className="size-full flex p-3">
-                                                            <div className={`text flex gap-3 p-3 ${screenWidth > 430 ? 'w-[50%] text-xs' : 'w-[100%] text-[10px]'} bg-black flex-col items-start justify-center`}>
-                                                                <h2 className="text-2xl text-start font-bold">{el.title}</h2>
-                                                                <p className="text-start">{el.overview}</p>
-                                                                <Link className='py-2 px-4 text-black rounded-xl bg-amber-300' href={`/details/${el.media_type}/${el.id}`}>view more</Link>
+                                                        <div className="size-full relative flex p-3">
+                                                            <div className={`text-collections absolute top-0 left-0 z-[2000] flex gap-3 p-3 size-full ${screenWidth > 430 ? 'text-xs' : 'text-[10px]'} flex-col items-start justify-center`}>
+                                                                <h2 className="text-2xl z-[2500] text-start font-bold">{el.title}</h2>
+                                                                <p className="text-start w-[70%] z-[2500]">{el.overview}</p>
+                                                                <Link className='py-2 px-4 z-[2500] text-black rounded-xl bg-amber-300' href={`/details/${el.media_type}/${el.id}`}>view more</Link>
                                                             </div>
-                                                            <div className="w-[50%] h-[100%] img relative" style={{ background: `url(https://image.tmdb.org/t/p/w1280${el.backdrop_path || el.poster_path})`,backgroundRepeat: 'no-repeat' , backgroundSize: 'cover', backgroundPosition: 'center' }}>
-
-                                                            </div>
+                                                            <Image 
+                                                                src={`https://image.tmdb.org/t/p/w1280${el.backdrop_path || el.poster_path}`}
+                                                                alt=""
+                                                                fill
+                                                            />
                                                         </div>
                                                     </SwiperSlide>
                                                 )
                                             })}
                                         </Swiper>
                                     </div>
-                                    <div className="w-[100%] flex justify-end h-[20%] text-sm">
+                                    <div className="w-[100%] flex justify-end pt-10 h-[20%] text-sm">
                                         <p className={`${screenWidth > 430 ? 'w-[50%]' : 'w-[100%]'}`}>{collection.overview}</p>
                                     </div>
                                 </div>
